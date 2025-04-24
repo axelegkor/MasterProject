924c15ac7006f877f43a91a5b18e75403b26ac88
axelegkor <axelegkor@gmail.com>
2025-04-24 11:11:02 +0200

    makefile and sync to idun main project

---
+++ FG-FNC/sync-to-idun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rsync -av --delete \
+  --filter='protect venv/' \
+  --filter='protect Makefile/' \
+  --filter='protect LICENSE/' \
+  --filter='protect logs/' \
+  --exclude=venv \
+  --exclude=__pycache__/ \
+  --exclude="*.pyc" \
+  --exclude=".git" \
+  --exclude="Makefile" \
+  --exclude="sync-to-idun.sh" \
+  ~/dev/MasterProject/FG-FNC/ \
+  axelle@idun.hpc.ntnu.no:/cluster/home/axelle/FG-FNC
